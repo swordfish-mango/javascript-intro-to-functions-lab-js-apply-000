a78214546602b420ffd1a243336585fcb03c385e
Marcus Shimotsu <marcus.shimotsu@gmail.com>
2021-01-03 17:11:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHitoGrandma(string) {
   if (string.toLowerCase() === string) {
-    console.log("I can't hear you!")
+    return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
-    console.log("YES INDEED!")
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
-    console.log("I love you, too.")
+    return "I love you, too."
   }
 }
